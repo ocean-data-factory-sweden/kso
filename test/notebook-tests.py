61b9d722b049644ac157a4e7e3dff72c814976c6
Jurie Germishuys <jurie.germishuys@combine.se>
2023-07-11 17:14:38 +0200

    Update utils + tests for t5, t6, t8

--- test/notebook-tests.py
+++ test/notebook-tests.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 26 16:20:56 2023
-
-@author: DiewertjeDekker
-
 
 This auto-test does not test if everything displays what it should display. 
 It mainly tests if everything still runs without giving any errors.
@@ -15,39 +11,26 @@ To run these tests manually, use pytest --disable-warnings test/notebook-tests.p
 """
 
 # --------------------The first 2 cells of every notebook-----------------------
+## Import Python packages
 import os
 import sys
 
-# for when we are running it on git, the yaml file will install the docker image. then we do need to go to the correct directory
-os.chdir("tutorials")
+if "kso_utils" not in sys.modules:
+    # for when we are running it on git, the yaml file will install the docker image. then we do need to go to the correct directory
+    os.chdir("tutorials")
+    sys.path.append("..")
+    import kso_utils.kso_utils
+    sys.modules["kso_utils"] = kso_utils.kso_utils
+
+import kso_utils.project_utils as p_utils
+from kso_utils.project import ProjectProcessor, MLProjectProcessor
 
 # https://stackoverflow.com/questions/15044447/how-do-i-unit-testing-my-gui-program-with-python-and-pyqt
 # for if we want to test the widgets
 
-## Import Python packages
-# try:
-if "kso_utils" not in sys.modules:
-    sys.path.append("..")
-    import kso_utils.kso_utils
-
-    sys.modules["kso_utils"] = kso_utils.kso_utils
-# except:
-
-# print("Installing latest version from PyPI...")
-#%pip install -q kso-utils
-
-
-# Import required modules
-import kso_utils.tutorials_utils as t_utils
-import kso_utils.project_utils as p_utils
-import kso_utils.yolo_utils as y_utils
-import kso_utils.widgets as kso_widgets
-from kso_utils.project import ProjectProcessor, MLProjectProcessor
-import kso_utils.server_utils as s_utils
-
-
 # -----------------Initiating of the project structure -------------------------
 # Initiate project's database
+
 project = p_utils.find_project(project_name="Template project")
 pp = ProjectProcessor(project)
 mlp = MLProjectProcessor(pp, test=True)
@@ -63,6 +46,8 @@ Both widgets can't be run due to a runtime error when nothing gets selected.
 
 
 def test_t1():
+    import kso_utils.widgets as kso_widgets
+
     # Display the map Tutorial 1
     pp.map_sites()
     # Retrieve and display movies
@@ -98,18 +83,20 @@ def test_t1():
     assert len(sites_sheet_df) == 5
 
 
-# #-------------Tutorial 2-------------------------------------------------------
+# -------------Tutorial 2-------------------------------------------------------
 # """
 # This tutorial will be removed
 # """
 
-# #-------------Tutorial 3-------------------------------------------------------
+# -------------Tutorial 3-------------------------------------------------------
 """
 ...
 """
 
 
 def test_t3(zoo_user, zoo_pass):
+    import kso_utils.widgets as kso_widgets
+
     # Connect to zooniverse w/ Github credential
     pp.get_zoo_info(zoo_cred=[zoo_user, zoo_pass])
     pp.movie_selected = "movie_1.mp4"
@@ -126,12 +113,17 @@ def test_t3(zoo_user, zoo_pass):
     assert len(pp.generated_clips) == 1
 
 
-# #-------------Tutorial 4-------------------------------------------------------
+# -------------Tutorial 4-------------------------------------------------------
 # """
 # It will not be tested if the actually uploading to Zooniverse works,
 # since we do not want to upload things to them all the time.
 # """
 def test_t4(zoo_user, zoo_pass):
+
+    import kso_utils.tutorials_utils as t_utils
+
+    # import kso_utils.widgets as kso_widgets
+
     pp.get_zoo_info(zoo_cred=[zoo_user, zoo_pass])
     pp.get_frames(test=True)
     pp.generate_zu_frames(test=True)
@@ -153,12 +145,18 @@ def test_t4(zoo_user, zoo_pass):
     assert len(pp.generated_frames) == 9
 
 
-# #-------------Tutorial 5-------------------------------------------------------
-# """
-# ...
-# """
+# # #-------------Tutorial 5-------------------------------------------------------
+# # """
+# # ...
+# # """
 def test_t5():
+
+    import kso_utils.tutorials_utils as t_utils
+    import kso_utils.yolo_utils as y_utils
+    import kso_utils.server_utils as s_utils
+
     # Generate current timestamp
+    import shutil
     from datetime import datetime
 
     dt = datetime.now()
@@ -171,6 +169,7 @@ def test_t5():
     mlp.setup_paths(test=True)
     exp_name = f"notebook_test_train_{dt}"
     project_path = os.path.join(mlp.output_path, mlp.project_name)
+    exp_path = os.path.join(project_path, exp_name)
 
     # Model training
     weights = y_utils.choose_baseline_model(mlp.output_path, test=True)
@@ -185,7 +184,7 @@ def test_t5():
         img_size=img_h.value,
     )
 
-    assert len(os.listdir(os.path.join(project_path, exp_name, "weights"))) == 2
+    assert len(os.listdir(os.path.join(exp_path, "weights"))) == 2
 
     # Model evaluation
     conf_thres = t_utils.choose_eval_params()
@@ -201,93 +200,123 @@ def test_t5():
 
     # assert len(os.listdir(f"{mlp.run_path}/labels")==0)
 
+    # Clean up residual files
+    shutil.rmtree(exp_path)
 
-# #-------------Tutorial 6-------------------------------------------------------
-# """
-# ...
-# """
-# def test_t6():
-# # Evaluation
-# model = mlp.choose_model()
-# download_dir = kso_widgets.choose_folder(".", "where to download the model")
-# artifact_dir = mlp.get_model(model.value, download_dir.selected)
-# source = kso_widgets.choose_footage(
-#     project=pp.project,
-#     server_connection=pp.server_connection,
-#     db_connection=pp.db_connection,
-#     start_path=pp.project.movie_folder
-#     if pp.project.movie_folder not in [None, "None"]
-#     else ".",
-#     folder_type="custom footage",
-# )
-# source_value = t_utils.process_source(source)
-# save_dir = kso_widgets.choose_folder(".", "runs output")
-# conf_thres = t_utils.choose_conf()
-# mlp.detect_yolov5(
-#     source=source_value,
-#     save_dir=save_dir.selected,
-#     conf_thres=conf_thres.value,
-#     artifact_dir=artifact_dir,
-# )
-# eval_dir = kso_widgets.choose_folder(
-#     save_dir.selected
-#     if "save_dir" in vars() and save_dir.selected is not None
-#     else ".",
-#     "runs output",
-# )
-# mlp.save_detections_wandb(conf_thres.value, model.value, eval_dir.selected)
-# viewer = y_utils.choose_files(eval_dir.selected)
-# train_dataset, val_dataset = mlp.get_dataset(model.value)
-# y_utils.get_data_viewer(os.path.join(train_dataset, "data/images"))
-# y_utils.get_data_viewer(os.path.join(val_dataset, "data/images"))
 
-# # Tracking individuals
-# mlp.track_individuals(
-#     source=source_value,
-#     artifact_dir=artifact_dir,
-#     eval_dir=eval_dir.selected,
-#     conf_thres=conf_thres.value,
-#     img_size=(540, 540),
-# )
+# -------------Tutorial 6-------------------------------------------------------
+"""
+...
+"""
+
+
+def test_t6():
+    import kso_utils.tutorials_utils as t_utils
+    import kso_utils.server_utils as s_utils
+
+    # Generate current timestamp
+    import shutil
+    from datetime import datetime
+
+    dt = datetime.now()
+    # Create a unique experiment name
+    exp_name = f"custom_{dt}".replace(" ", "_").replace(".", "_").replace(":", "-")
+    mlp.output_path = "../test/test_output"
+    project_path = os.path.join(mlp.output_path, mlp.project_name)
+    exp_path = os.path.join(project_path, exp_name)
+    # Evaluation
+    s_utils.get_ml_data(project, test=True)
+    model = mlp.choose_model().options[-1][1]
+    download_dir = mlp.output_path
+    artifact_dir = mlp.get_model(model, download_dir)
+    source = os.path.join("../test/test_output", mlp.project.ml_folder, "images")
+    save_dir = project_path
+    conf_thres = t_utils.choose_conf()
+    mlp.detect_yolov5(
+        exp_name=exp_name,
+        source=source,
+        save_dir=save_dir,
+        conf_thres=conf_thres.value,
+        artifact_dir=artifact_dir,
+    )
+    eval_dir = exp_path
+    mlp.save_detections_wandb(conf_thres.value, model, eval_dir)
+
+    # Create unique tracking experiment name
+    track_exp_name = (
+        f"tracker_test_{dt}".replace(" ", "_").replace(".", "_").replace(":", "-")
+    )
+
+    # Tracking individuals
+    mlp.track_individuals(
+        name=track_exp_name,
+        source=source,
+        artifact_dir=artifact_dir,
+        eval_dir=eval_dir,
+        conf_thres=conf_thres.value,
+        img_size=(640, 640),
+    )
+    shutil.rmtree(exp_path)
 
 
 # #-------------Tutorial 7-------------------------------------------------------
-# """
-# ...
-# """
-
-# def test_t7():
-# model = mlp.choose_model()
-# download_dir = kso_widgets.choose_folder(".", "downloaded model")
-# artifact_dir = mlp.get_model(model.value, download_dir.selected)
-# ACCESS_TOKEN = ""
-# archive_dir = kso_widgets.choose_folder(".", "archive for upload")
-# depo_id = zenodo_utils.upload_archive(ACCESS_TOKEN, artifact_dir=artifact_dir)
-# upload_title = t_utils.choose_text("title")
-# upload_description = t_utils.choose_text("description")
-# authors = t_utils.WidgetMaker()
-# zenodo_utils.add_metadata_zenodo_upload(
-#     ACCESS_TOKEN,
-#     depo_id,
-#     upload_title.value,
-#     upload_description.value,
-#     authors.author_dict,
-# )
+"""
+This tutorial is still in a very early stage and mostly uses widgets directly from widgets.py and
+API functionality from Zenodo. Tests should be added in future as its capabilities expand. 
+"""
 
 
-# #-------------Tutorial 8-------------------------------------------------------
-# """
-# ...
-# """
-# pp.choose_workflows(False, zoo_cred=zoo_cred)
+def test_t7():
+    pass
 
-# workflow_checks = {'Workflow name: #0': 'Development workflow',
-#  'Subject type: #0': 'clip',
-#  'Minimum workflow version: #0': 1.0}
 
-# class_df = pp.get_classifications(
-#     workflow_checks,
-#     pp.zoo_info["workflows"],
-#     workflow_checks["Subject type: #0"],
-#     pp.zoo_info["classifications"],
-# )
+# -------------Tutorial 8-------------------------------------------------------
+"""
+...
+"""
+
+
+def test_t8(zoo_user, zoo_pass):
+    import kso_utils.widgets as kso_widgets
+
+    # Connect to zooniverse w/ Github credential
+    pp.get_zoo_info(zoo_cred=[zoo_user, zoo_pass])
+    pp.choose_workflows(generate_export=False, zoo_cred=[zoo_user, zoo_pass])
+
+    workflow_checks = {
+        "Workflow name: #0": "Development workflow",
+        "Subject type: #0": "clip",
+        "Minimum workflow version: #0": 1.0,
+    }
+
+    class_df = pp.get_classifications(
+        workflow_checks,
+        pp.zoo_info["workflows"],
+        workflow_checks["Subject type: #0"],
+        pp.zoo_info["classifications"],
+    )
+
+    assert len(class_df) == 9
+
+    agg_params = kso_widgets.choose_agg_parameters(workflow_checks["Subject type: #0"])
+
+    agg_df, raw_df = pp.process_classifications(
+        pp.zoo_info["classifications"],
+        workflow_checks["Subject type: #0"],
+        agg_params,
+        summary=False,
+    )
+
+    assert len(agg_df) == 3
+
+    # Run the preparation script
+    mlp.output_path = "../test/test_output"
+    # This test does not work yet as the Template Project has no frame classifications
+    # TODO: Add frame classifications and adjust workflow name and subject type above
+    # mlp.prepare_dataset(
+    #    agg_df=agg_df,
+    #    out_path=mlp.output_path,
+    #    img_size=(640, 640),
+    #    perc_test=0.2,
+    #    test=True,
+    # )
