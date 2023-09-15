992c37858a855a6eece2826b0ca159391cd76523
Diewertje11 <diewertje@kpnplanet.nl>
2023-09-15 17:14:38 +0200

    [+whitelist, detect-unused-code.yml]
    The whitelist.py is a file in which we can state all errors Vulture
    reports that we have checked and want to keep. Vulture states many false
    positives and therefore this is needed.
    
    With this file we can also have the vulture CI check fail when it
    actually detects unused code.
    
    So the idea is to check everything and put all false positives in the
    whitelist. Once in the so much time we should clear the whitelist and
    recheck everything.

---
+++ whitelist.py
@@ -0,0 +1,5 @@
+# This file contains everything Vulture should ignore.
+# So if we have checked the function/variable/method is actually used, we can put it here.
+# Put them all as: (without the '#'
+
+#drawBoxes  # unused function (kso_utils\frame_utils.py:19)
