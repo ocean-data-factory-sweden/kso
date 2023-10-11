0ff0fda868b215e3a7bf9d2440d205cc99e1af71
Jurie Germishuys <jurie.germishuys@combine.se>
2023-10-11 14:54:53 +0200

    Install older version of ultralytics for yolov5

--- kso_utils/project.py
+++ kso_utils/project.py
@@ -1,6 +1,7 @@
 # base imports
 import os
 import sys
+import subprocess
 import logging
 import asyncio
 import wandb
@@ -1030,7 +1031,7 @@ class MLProjectProcessor(ProjectProcessor):
         self.best_model_path = None
         self.model_type = 1  # set as 1 for testing
         self.train, self.run, self.test = (None,) * 3
-        
+
         self.registry = "wandb"
         if "MLFLOW_TRACKING_URI" in os.environ:
             if os.environ["MLFLOW_TRACKING_URI"] is not None:
@@ -1054,11 +1055,13 @@ class MLProjectProcessor(ProjectProcessor):
             )
         )
         # Import model models for backwards compatibility
-        self.modules.update(
-            g_utils.import_model_modules(
-                ["yolov5.train", "yolov5.val", "yolov5.detect"],
+        if self.registry == "wandb":
+            subprocess.run(["pip", "install", "ultralytics==8.0.100"])
+            self.modules.update(
+                g_utils.import_model_modules(
+                    ["yolov5.train", "yolov5.detect", "yolov5.val"],
+                )
             )
-        )
 
         self.team_name = "koster"
 
