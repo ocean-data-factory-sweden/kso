daaeb68d4c94425fb53baa4c0fb9decc2df5d6c4
Jurie Germishuys <jurie.germishuys@combine.se>
2024-02-26 14:49:58 +0100

    Use os instead of root for chmod

--- kso_utils/zooniverse_utils.py
+++ kso_utils/zooniverse_utils.py
@@ -1750,7 +1750,7 @@ def modify_frames(
         if not mod_frames_folder.exists():
             mod_frames_folder.mkdir(parents=True, exist_ok=True)
             # Recursively add permissions to folders created
-            [root.chmod(0o777) for root, _, _ in os.walk(mod_frames_folder)]
+            [os.chmod(0o777) for root, _, _ in os.walk(mod_frames_folder)]
 
         #### Modify the clips###
         # Read each clip and modify them (showing a progress bar)
