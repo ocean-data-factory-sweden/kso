104cff61ee33e8ea32c12812e60684294eae9c38
Diewertje11 <diewertje.dekker@combine.se>
2025-07-07 14:19:29 +0200

    [test_registries.py] run on all py files in folder
    The previous commit only had the refactor, now this code is changed to
    actually run via pytest in the CI. For any new registry, it will
    automatically run these tests, since it runs on all files inside the
    registry folder.

--- kso_utils/registries/test_registries.py
+++ kso_utils/registries/test_registries.py
@@ -1,21 +1,29 @@
-def validate_registry(utils, required_functions):
+import importlib
+import pkgutil
+import pytest
+import kso_utils.registries
+
+
+def get_registry_modules():
+    """
+    A function to get all python files that are stored inside this registry folder.
+    """
+    package = kso_utils.registries
+    prefix = package.__name__ + "."
+    for _, modname, ispkg in pkgutil.iter_modules(package.__path__, prefix):
+        if not ispkg and not modname.split(".")[-1].startswith("test_"):
+            yield importlib.import_module(modname)
+
+
+@pytest.mark.parametrize("registry_module", list(get_registry_modules()))
+def test_validate_registry(registry_module):
     """
     We want the code to be more modular, so that we can swap out different model training packages,
     or training tracking services for each other. To have all these modular components, all of these will
     have their own utils file, which needs to contain a specified set of functions.
     This function will check a utils file if it contains all the required functions.
     """
-    # Validate they exist in the module
-    missing = [func for func in required_functions if not hasattr(utils, func)]
-    if missing:
-        raise ImportError(
-            f"Module 'kso_utils.{utils}_utils' is missing required functions: {missing}"
-        )
-
-
-validate_registry(
-    self.registry_utils,
-    [
+    required_functions = [
         "init",
         "start_run",
         "close_run",
@@ -23,5 +31,12 @@ validate_registry(
         "choose_model",
         "get_model",
         "get_dataset",
-    ],
-)
+    ]
+    # Validate they exist in the module
+    missing = [
+        func for func in required_functions if not hasattr(registry_module, func)
+    ]
+    if missing:
+        raise ImportError(
+            f"Module '{registry_module}' is missing required functions: {missing}"
+        )
