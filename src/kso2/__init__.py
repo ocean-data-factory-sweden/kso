475e6b8f3c85b32cece193993337605c8b2bcc32
nithador <tomas.martinovic@vsb.cz>
2025-12-17 10:24:24 +0100

    format black

--- src/kso2/__init__.py
+++ src/kso2/__init__.py
@@ -3,15 +3,14 @@ from .trainer import YOLOv11MLflowModel, training_model, export_experiment
 from .serving_utils import start_mlflow_server, stop_mlflow_server
 
 __all__ = [
-    "Project", 
-    "create_project", 
-    "add_model", 
-    "add_data", 
+    "Project",
+    "create_project",
+    "add_model",
+    "add_data",
     "add_Biigle_data",
-    "YOLOv11MLflowModel", 
-    "training_model", 
+    "YOLOv11MLflowModel",
+    "training_model",
     "export_experiment",
-    "start_mlflow_server", 
-    "stop_mlflow_server"
+    "start_mlflow_server",
+    "stop_mlflow_server",
 ]
-
