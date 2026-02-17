32cb7630a1005a37d0adfd7666e39305132a80fe
Ghaith <ghaith@vpnp119.vsb.cz>
2026-02-17 16:36:25 +0100

    add tests for the creat project config file

---
+++ test/test_project.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from kso2 import create_project, Project
+import pytest
+
+# @pytest.fixture()
+# def project():
+#     project_temp=Project(
+#                     project_name="Test project 1",
+#                     project_path="~/desktop/",
+#                     model_name="best model",
+#                     model_path="yolov8n.pt",
+#                 )
+#     return project_temp
+
+
+def test_init_create_project():
+    """
+    test project config function creat_project
+    """
+    project_test = create_project(
+        project_name="Test project 1",
+        project_path="~/desktop/",
+        model_name="best model",
+        weights_path="yolov8n.pt",
+    )
+    assert isinstance(
+        project_test, Project
+    ), f"{project_test} is not set correctly, they should be an instance of {Project} "
