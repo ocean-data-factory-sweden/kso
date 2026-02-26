381d016a804f356f856308052a65c6e04a6d78c8
Tuomas Rossi <tuomas.rossi@csc.fi>
2026-02-26 13:51:39 +0200

    Fix unzipping

--- edito/download_playday_data.sh
+++ edito/download_playday_data.sh
@@ -5,6 +5,6 @@ echo -n "Downloading..." && \
 curl -sL "https://www.dropbox.com/scl/fo/3moo7nw5ab52qqsypmlx6/AFG5CVzmQctYGGKRfbYUSSM?rlkey=yntdqhn14fsth86b5xwu4f9bz&st=m14ospjo&dl=1" -o $f && \
 echo " OK!" && \
 echo -n "Extracting..." && \
-unzip -q $f && \
+unzip -q $f -x "/" && \
 echo " OK!" && \
 rm -f $f
